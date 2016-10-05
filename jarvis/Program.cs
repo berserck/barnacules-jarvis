8a0ccdf61fa39c57ac9de1ed347b6ff449e73d33
Pedro Parracho <pedro.parracho@deram.com>
2016-10-05 14:35:20 +0200

    Added message to report uptime, and spceial message for 100% CPU

--- jarvis/Program.cs
+++ jarvis/Program.cs
@@ -16,7 +16,8 @@ namespace jarvis
     {
       // This will greet the user in the default voice
       SpeechSynthesizer synth = new SpeechSynthesizer();
-      //synth.Speak("Welcome to jarvis version one point o");
+      synth.SelectVoiceByHints(VoiceGender.Neutral);
+      synth.Speak("Welcome to jarvis version one point oh");
 
 
       #region My Performance Counters
@@ -26,13 +27,29 @@ namespace jarvis
       PerformanceCounter perfMemCount = new PerformanceCounter("Memory", "Available MBytes");
       // This wuill get system uptime in s
       PerformanceCounter perfUptimeCount = new PerformanceCounter("System", "System Up Time");
+
+      // need to run Next value once to initialize the counter, if not first value is 0
+      perfCPUCount.NextValue();
+      perfMemCount.NextValue();
+      perfUptimeCount.NextValue();
+
       #endregion
 
+      TimeSpan uptimeSpan = TimeSpan.FromSeconds(perfUptimeCount.NextValue());
+      string systemUptimeMessage = string.Format("The current system up time is {0} days, {1} hours {2} minutes and {3} seconds",
+        (int)uptimeSpan.TotalDays,
+        uptimeSpan.Hours,
+        uptimeSpan.Minutes,
+        uptimeSpan.Seconds
+        );
+      Console.WriteLine(systemUptimeMessage);
+
+      synth.Speak(systemUptimeMessage);
       // infinite loop
       while (true)
       {
 
-        int currentCPUPercentage =  (int)perfCPUCount.NextValue();
+        int currentCPUPercentage = (int)perfCPUCount.NextValue();
         int currentAvailableMemory = (int)perfMemCount.NextValue();
 
         Console.WriteLine("CPU Load     : {0}%", currentCPUPercentage);
@@ -41,13 +58,23 @@ namespace jarvis
 
         if (currentCPUPercentage > 80)
         {
-          string cpuLoadVocalMessage = String.Format("The current CPU load is {0}", currentCPUPercentage);
-          synth.Speak(cpuLoadVocalMessage);
-
+          if (currentCPUPercentage == 100)
+          {
+            synth.SelectVoiceByHints(VoiceGender.Female);
+            string cpuLoadVocalMessage = String.Format("Warning: CPU at max", currentCPUPercentage);
+            synth.Speak(cpuLoadVocalMessage);
+          }
+          else
+          {
+            synth.SelectVoiceByHints(VoiceGender.Male);
+            string cpuLoadVocalMessage = String.Format("The current CPU load is {0}", currentCPUPercentage);
+            synth.Speak(cpuLoadVocalMessage);
+          }
         }
 
         if (currentAvailableMemory < 1024)
         {
+          synth.SelectVoiceByHints(VoiceGender.Male);
           string memVocalMessage = String.Format("You currently have {0} megabytes of memory available", currentAvailableMemory);
           synth.Speak(memVocalMessage);
         }
