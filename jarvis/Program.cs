d8bcd350be74c20d36085a509da7a17395688d80
Pedro Parracho <pedro.parracho@deram.com>
2016-10-05 13:58:27 +0200

    Added cpu and memory monitoring, adde reference to speach

---
+++ jarvis/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Diagnostics;
+using System.Threading;
+
+
+namespace jarvis
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      //This will pull the current CPU load in percentage
+      PerformanceCounter perfCPUCount = new PerformanceCounter("Processor Information", "% Processor Time", "_Total");
+      PerformanceCounter perfMemCount = new PerformanceCounter("Memory", "Available MBytes");
+
+      // infinite loop
+      while (true)
+      {
+        Thread.Sleep(1000);
+        Console.WriteLine("CPU Load: {0}%", perfCPUCount.NextValue());
+        Console.WriteLine("Available Mem: {0}MB", perfMemCount.NextValue());
+
+      }
+    }
+  }
+}
